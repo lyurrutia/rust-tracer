f1ec1f5a667d54082ecb9f245e9f000fac559ee5
Lyra Urrutia Reyes <murrutia@ucsd.edu>
2024-11-21 19:01:48 -0800

    Implemented beginning of unit tests for vectors

--- src/vec3.rs
+++ src/vec3.rs
@@ -13,6 +13,11 @@ impl Vec3 {
         Vec3 { e : [0.0,0.0,0.0] }
     }
 
+    // Function to create a vector from any numbers castable into f64
+    pub fn new_vec<T:Into<f64> + Copy>(x:T, y:T, z:T) -> Vec3 {
+        Vec3 { e : [x.into(), y.into(), z.into()] }
+    }
+
     pub fn x(self) -> f64 { return self.e[0] }
     pub fn y(self) -> f64 { return self.e[1] }
     pub fn z(self) -> f64 { return self.e[2] }
@@ -45,5 +50,39 @@ impl IndexMut<usize> for Vec3 {
 
     fn index_mut(&mut self, index:usize) -> &mut Self::Output { 
         return &mut self.e[index] 
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn vec_empty() {
+        let blank_vec = Vec3::new_empty();
+        assert_eq!(blank_vec.e[0],  0.0);
+        assert_eq!(blank_vec.e[1],  0.0);
+        assert_eq!(blank_vec.e[2],  0.0);
+        vec_equality(&blank_vec);
+        vec_negation(&blank_vec);
+
+        let pos_vec = Vec3::new_vec(1, 2, 3);
+        assert_eq!(pos_vec.e[0],  1.0);
+        assert_eq!(pos_vec.e[1],  2.0);
+        assert_eq!(pos_vec.e[2],  3.0);
+        vec_equality(&pos_vec);
+        vec_negation(&pos_vec);
+    }
+
+    fn vec_equality(test_vec: &Vec3) {
+        assert_eq!(test_vec.x(),  test_vec.e[0]);
+        assert_eq!(test_vec.y(),  test_vec.e[1]);
+        assert_eq!(test_vec.z(),  test_vec.e[2]);
+    }
+
+    fn vec_negation(test_vec: &Vec3) {
+        let negtest_vec = -*test_vec;
+        assert_eq!(negtest_vec.x(),  -test_vec.e[0]);
+        assert_eq!(negtest_vec.y(),  -test_vec.e[1]);
+        assert_eq!(negtest_vec.z(),  -test_vec.e[2]);
     }
 }
